7ab8962bf769cd4cf37b53c7fc14a26cd1068d20
brainbugg <brainbugger23@gmail.com>
2016-11-22 17:06:16 +0545

    Signed-off-by: brainbugg <brainbugger23@gmail.com>

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*IMMEDIATELY INVOKING FUNCTION*/
+// Runs automatically when the page loads
+
+(function()
+{
+	// go find turtlefacts module and return it to me
+	angular.module("turtleFacts", [/*dependencies*/]);
+
+})();
